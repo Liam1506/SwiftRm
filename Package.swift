826765c9f45058b941fa7b7f578c8aa64db161a6
Liam Wittig <liam.wittig.hn@gmail.com>
2026-04-05 12:40:51 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftRm",
+    platforms: [
+            .iOS(.v26),     
+            .iOS(.v18),
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftRm",
+            targets: ["SwiftRm"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftRm"
+        ),
+        .testTarget(
+            name: "SwiftRmTests",
+            dependencies: ["SwiftRm"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
